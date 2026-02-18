d5b11a682f69ca923dbddf774db6b6c13c94d015
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-02-18 18:39:44 +0300

    .

--- AIThumb/client/src/components/Navbar.tsx
+++ AIThumb/client/src/components/Navbar.tsx
@@ -3,10 +3,11 @@ import { useState } from "react";
 import { motion } from "motion/react";
 import { navlinks } from "../data/navlinks";
 import type { INavLink } from "../types";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
+    const navigate = useNavigate()
 
     return (
         <>
@@ -16,20 +17,22 @@ export default function Navbar() {
                 viewport={{ once: true }}
                 transition={{ type: "spring", stiffness: 250, damping: 70, mass: 1 }}
             >
-                <a href="https://prebuiltui.com?utm_source=pixels">
-                    <img className="h-8.5 w-auto" src="/assets/logo.svg" alt="logo" width={130} height={34} />
-                </a>
+                <Link to='/'>
+                <img src="/logo.png" alt="logo" className="h-8.5 w-auto"/>
+                </Link>
 
                 <div className="hidden md:flex items-center gap-8 transition duration-500">
-                    {navlinks.map((link: INavLink) => (
-                        <NavLink key={link.name} to={link.href} className="hover:text-pink-500 transition">
-                            {link.name}
-                        </NavLink>
-                    ))}
+
+                    <Link to='/' className="hover:text-pink-500 transition">Anasayfa</Link>
+                    <Link to='/generate' className="hover:text-pink-500 transition">Oluşturmaya Başla</Link>
+                    <Link to='/my-generation' className="hover:text-pink-500 transition">Oluşturduklarım</Link>
+                    <Link to='#' className="hover:text-pink-500 transition">İletişim</Link>
+
+                   
                 </div>
 
-                <button className="hidden md:block px-6 py-2.5 bg-pink-600 hover:bg-pink-700 active:scale-95 transition-all rounded-full">
-                    Start free trial
+                <button onClick={()=> navigate('/login')} className="hidden md:block px-6 py-2.5 bg-pink-600 hover:bg-pink-700 active:scale-95 transition-all rounded-full">
+                    Başla
                 </button>
                 <button onClick={() => setIsOpen(true)} className="md:hidden">
                     <MenuIcon size={26} className="active:scale-90 transition" />
