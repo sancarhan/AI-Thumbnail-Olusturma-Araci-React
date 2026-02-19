e03ddf441611d2ee84ebac21317bd0ab6cf6bd43
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-02-19 12:48:53 +0300

    .

--- AIThumb/client/src/components/Navbar.tsx
+++ AIThumb/client/src/components/Navbar.tsx
@@ -40,11 +40,11 @@ export default function Navbar() {
             </motion.nav>
 
             <div className={`fixed inset-0 z-100 bg-black/40 backdrop-blur flex flex-col items-center justify-center text-lg gap-8 md:hidden transition-transform duration-400 ${isOpen ? "translate-x-0" : "-translate-x-full"}`}>
-                {navlinks.map((link: INavLink) => (
-                    <NavLink key={link.name} to={link.href} onClick={() => setIsOpen(false)}>
-                        {link.name}
-                    </NavLink>
-                ))}
+                    <Link onClick={() => setIsOpen(false)} to='/'>Anasayfa </Link>
+                    <Link onClick={() => setIsOpen(false)} to='/generate'>Oluşturmaya Başla</Link>
+                    <Link onClick={() => setIsOpen(false)} to='/my-generation' >Oluşturduklarım</Link>
+                    <Link onClick={() => setIsOpen(false)} to='#'>İletişim</Link>
+                    <Link onClick={() => setIsOpen(false)} to='/login'>Giriş</Link>
                 <button onClick={() => setIsOpen(false)} className="active:ring-3 active:ring-white aspect-square size-10 p-1 items-center justify-center bg-pink-600 hover:bg-pink-700 transition text-white rounded-md flex">
                     <XIcon />
                 </button>
