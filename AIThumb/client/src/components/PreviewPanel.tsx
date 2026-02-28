719f553046c821e79ba2dbb3678374f86bdf0487
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-03-01 00:50:42 +0300

    .

---
+++ AIThumb/client/src/components/PreviewPanel.tsx
@@ -0,0 +1,53 @@
+import {type AspectRatio, type IThumbnail } from "../assets/assets"
+import { DownloadIcon, Loader2Icon } from "lucide-react";
+
+
+const PreviewPanel = ({thumbnail, isLoading, aspectRatio}: {thumbnail: IThumbnail, isLoading: boolean; aspectRatio: AspectRatio}) => {
+
+ const aspectClasses = {
+  '16:9': 'aspect-video',
+  '1:1': 'aspect-square',
+  '9:1': 'aspect-[9/16]',
+ } as Record<AspectRatio, string>;
+
+ const onDownload = ()=>{
+  if(!thumbnail?.image_url) return;
+  window.open(thumbnail.image_url, '_blank')
+ }
+
+  return (
+    <div className="relative mx-auto w-full max-w-2xl">
+      <div className={`relative overflow-hidden ${aspectClasses[aspectRatio]}`}>
+       {/* Loading */}
+       {isLoading && (
+        <div className="absolute inset-0 flex flex-col items-center justify-center gap-4 bg-black/25">
+         <Loader2Icon className="size-8 animate-spin text-zinc-400"/>
+         <div className="text-center">
+          <p className="text-sm font-medium text-zinc-200">Thumbnail Oluşturuluyor...</p>
+          <p className="mt-1 text-xs text-zinc-400">Yaklaşık 10-20 saniye sürebilir</p>
+         </div>
+        </div>
+       )}
+
+       {/* Image */}
+       {!isLoading && thumbnail?.image_url && (
+        <div className="group relative h-full w-full">
+         <img src={thumbnail?.image_url} alt={thumbnail.title} className="h-full w-full object-cover"/>
+
+         <div className="absolute inset-0 flex items-end justify-center bg-black/10 opacity-0 transition-opacity group-hover:opacity-100">
+          <button onClick={onDownload} type="button" className="mb-6 flex items-center gap-2 rounded-md px-5 py-2.5 text-xs font-medium transition bg-white/30 ring-2 ring-white/40 backdrop-blur hover: scale-105 active:scale-95">
+           <DownloadIcon className="size-4"/>
+           Thumbnail'i İndir
+          </button>
+         </div>
+        </div>
+       )}
+
+       {/* boş durum */}
+
+      </div>
+    </div>
+  )
+}
+
+export default PreviewPanel
