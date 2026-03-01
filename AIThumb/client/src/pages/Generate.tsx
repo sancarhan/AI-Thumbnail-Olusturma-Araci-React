7b167e08240305870ab163a4c2db3f59bf834edf
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-03-02 00:08:52 +0300

    .

--- AIThumb/client/src/pages/Generate.tsx
+++ AIThumb/client/src/pages/Generate.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
-import { colorSchemes, type AspectRatio, type IThumbnail, type ThumbnailStyle } from "../assets/assets";
+import { colorSchemes, dummyThumbnails, type AspectRatio, type IThumbnail, type ThumbnailStyle } from "../assets/assets";
 import SoftBackdrop from "../components/SoftBackdrop";
 import AspectRatioSelector from "../components/AspectRatioSelector";
 import StyleSelector from "../components/StyleSelector";
@@ -23,6 +23,32 @@ const Generate = () => {
 
  const [styleDropdownOpen, setStyleDropdownOpen] = useState(false)
 
+ const handleGenerate = async () => {
+
+ }
+
+ const fetchThumbnail = async ()=>{
+
+  if (id) {
+    const thumbnail : any = dummyThumbnails.find((thumbnail)=> thumbnail._id === id);
+    setThumbnail(thumbnail)
+    setAdditionalDetails(thumbnail.user_prompt)
+    setTitle(thumbnail.title)
+    setColorSchemeId(thumbnail.color_scheme)
+    setAspectRatio(thumbnail.aspect_ratio)
+    setStyle(thumbnail.style)
+    setLoading(false)
+  }
+
+ }
+
+ useEffect(()=>{
+
+  if (id) {
+    fetchThumbnail()
+  }
+
+ },[id])
 
   return (
     <>
@@ -70,7 +96,7 @@ const Generate = () => {
 
           {/* Buton */}
           {!id && (
-            <button className="text-[15px] w-full py-3.5 rounded-xl font-medium bg-linear-to-b from-blue-500 to-blue-600 hover:from-blue-700 disabled:cursor-not-allowed transition-colors">
+            <button onClick={handleGenerate} className="text-[15px] w-full py-3.5 rounded-xl font-medium bg-linear-to-b from-blue-500 to-blue-600 hover:from-blue-700 disabled:cursor-not-allowed transition-colors">
               {loading ? 'Oluşturuluyor...' : 'Thumbnail Oluşturuldu'}
             </button>
           )}
