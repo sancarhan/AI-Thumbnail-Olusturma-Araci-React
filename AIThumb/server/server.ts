39c216c47092cdda48c77d51619a671ed5c8fc40
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-03-05 00:14:16 +0300

    .

--- AIThumb/server/server.ts
+++ AIThumb/server/server.ts
@@ -2,6 +2,15 @@ import "dotenv/config";
 import express, { Request, Response } from 'express';
 import cors from "cors";
 import connectDB from "./configs/db.js";
+import session from 'express-session'
+import MongoStore from 'connect-mongo'
+
+declare module 'express-session' {
+    interface SessionData{
+        isLoggedIn: boolean;
+        userId: string;
+    }
+}
 
 await connectDB()
 
@@ -13,7 +22,24 @@ app.use(cors())
 app.use(express.json());
 
 
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:5173','http://localhost:3000'],
+    credentials: true
+}))
+
+app.use(session({
+    secret: process.env.SESSION_SECRET as string,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {maxAge: 1000 * 60 * 60 * 24 * 7}, // 7 GÜN
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI as string,
+        collectionName: 'sessions'
+    })
+     
+
+}))
+
 app.use(express.json())
 
 app.get('/', (req: Request, res: Response) => {
